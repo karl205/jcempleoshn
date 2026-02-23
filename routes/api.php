d028675e5061519ce98e5ce64ccd8f2a74ea97da
Paola <mizanvs014@gmail.com>
2026-02-22 19:21:14 -0600

    Integraciòn con frontend

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::get('/ping', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'API conectada correctamente'
+    ]);
+});
+
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
